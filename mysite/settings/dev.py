fb0b34b1a68516681ea84e73caf833f651813153
AdrianaIto <adriana@sbworks.jp>
2021-09-29 11:04:05 +0900

    first commit

---
+++ mysite/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-89h!&co+ne56puo-4^8mod8i=t!6d6&&@@e)2$4j*bo-w&m$&6'
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ['*'] 
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
